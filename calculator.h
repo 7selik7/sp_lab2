696d3563b3cee3f5ad5a38f192b4088b44a8118b
7selik7 <evgenkoli8@gmail.com>
2023-11-21 21:55:57 +0200

    some changs

--- calculator.h
+++ calculator.h
@@ -6,6 +6,12 @@ class Calculator
     public:
         int Add (int, int);
         int Mul (double, double);
+        // test-comment 1
+        // test-comment 2
+        // test-comment 3
+        // test-comment 4
+        // test-comment 5
+
 };
 
 #endif//CALCULATOR_H
